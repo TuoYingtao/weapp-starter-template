b1c2d3b7bb8c91194e8eabb5a021a30d6af68186
TuoYingtao <tuoyingtao@163.com>
2024-03-22 09:55:27 +0800

    optimize: Git commit 代码格式化工具

--- config/env.js
+++ config/env.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "envType": "dev",
-  "name": "weapp-starter-template",
-  "appID": "wx82ccbac5ad7ec480",
-  "version": "1.0.0",
-  "remoteUrl": "https://guapi.liangjihui.com",
-  "cos": {
-    "Bucket": "",
-    "Region": "",
-    "previewPictureUrl": ""
+  envType: 'dev',
+  name: 'weapp-starter-template',
+  appID: 'wx82ccbac5ad7ec480',
+  version: '1.0.0',
+  remoteUrl: 'https://guapi.liangjihui.com',
+  cos: {
+    Bucket: '',
+    Region: '',
+    previewPictureUrl: ''
   }
-}
+};
--- config/router.js
+++ config/router.js
@@ -1,13 +1,5 @@
 module.exports = {
-  "defaultPage": "pages/index/index",
-  "mainPage": [
-    "pages/index/index",
-    "pages/logs/logs"
-  ],
-  "pageAll": [
-    "pages/index/index",
-    "pages/logs/logs",
-    "pages/userModule/index",
-    "pages/userModule/user_info/index"
-  ]
-}
+  defaultPage: 'pages/index/index',
+  mainPage: ['pages/index/index', 'pages/logs/logs'],
+  pageAll: ['pages/index/index', 'pages/logs/logs', 'pages/userModule/index', 'pages/userModule/user_info/index']
+};
