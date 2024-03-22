fdf956a83393ac34e9b632386e117c3d03cff4c3
TuoYingtao <tuoyingtao@163.com>
2024-03-22 11:23:57 +0800

    build: :package: 优化git commit信息格式构建工具

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,32 @@
-/**
- * feature：新功能
- * update：更新某功能
- * fixbug：修补某功能的bug
- * refactor：重构某个功能
- * optimize: 优化构建工具或运行时性能
- * style：仅样式改动
- * docs：仅文档新增/改动
- * chore：构建过程或辅助工具的变动
- */
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const scopes = fs
+  .readdirSync(path.resolve(__dirname, ''), { withFileTypes: true })
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name.replace(/s$/, ''));
+
+// precomputed scope
+const scopeComplete = execSync('git status --porcelain || true')
+  .toString()
+  .trim()
+  .split('\n')
+  .find(r => ~r.indexOf('M  src'))
+  ?.replace(/(\/)/g, '%%')
+  ?.match(/src%%((\w|-)*)/)?.[1]
+  ?.replace(/s$/, '');
+
+/** @type {import('cz-git').UserConfig} */
 module.exports = {
+  ignores: [commit => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feature', 'update', 'fixbug', 'refactor', 'optimize', 'style', 'docs', 'chore']],
+    'type-enum': [
+      2,
+      'always',
+      ['feature', 'update', 'fixbug', 'refactor', 'optimize', 'style', 'docs', 'test', 'build', 'ci', 'chore']
+    ],
     'type-case': [0],
     'type-empty': [0],
     'scope-empty': [0],
@@ -19,5 +34,106 @@ module.exports = {
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never'],
     'header-max-length': [0, 'always', 72]
+  },
+  prompt: {
+    alias: {
+      f: 'docs: fix typos',
+      r: 'docs: update README',
+      s: 'style: update code format',
+      b: 'build: bump dependencies',
+      c: 'chore: update config'
+    },
+    customScopesAlign: !scopeComplete ? 'top' : 'bottom',
+    defaultScope: scopeComplete,
+    scopes: [...scopes, 'mock'],
+    allowEmptyIssuePrefixs: true,
+    allowCustomIssuePrefixs: true,
+    messages: {
+      type: '请选择提交类型：',
+      scope: '选择一个提交范围（可选）:',
+      customScope: '请输入自定义的提交范围：',
+      subject: '请简要描述提交（必填）：',
+      body: '请输入详细描述（可选）:',
+      breaking: '列举非兼容性重大的变更。使用 "|" 换行（可选）：',
+      footerPrefixsSelect: '选择关联issue前缀（可选）:',
+      customFooterPrefixs: '输入自定义issue前缀:',
+      footer: '请输入要关闭的issue 例如: #31, #I3244（可选）：',
+      confirmCommit: '确认使用以上信息提交？（y/n）'
+    },
+    types: [
+      { value: 'feature', name: 'feature:      ✨ 新增功能 | A new feature', emoji: ':sparkles:' },
+      { value: 'update', name: 'update:       🔧 更新功能 | A function update', emoji: ':update:' },
+      { value: 'fixbug', name: 'fixbug:       🐛 修复缺陷 | A bug fixbug', emoji: ':bug:' },
+      {
+        value: 'refactor',
+        name: 'refactor:  ♻️  代码重构 | A code change that neither fixes a bug nor adds a feature',
+        emoji: ':recycle:'
+      },
+      {
+        value: 'optimize',
+        name: 'optimize:      ⚡️ 性能提升 | A code change that improves optimize',
+        emoji: ':zap:'
+      },
+      {
+        value: 'style',
+        name: 'style:     💄 代码格式 | Changes that do not affect the meaning of the code',
+        emoji: ':lipstick:'
+      },
+      {
+        value: 'docs',
+        name: 'docs:      📝 文档更新 | Documentation only changes',
+        emoji: ':memo:'
+      },
+      {
+        value: 'test',
+        name: 'test:      ✅ 测试相关 | Adding missing tests or correcting existing tests',
+        emoji: ':white_check_mark:'
+      },
+      {
+        value: 'build',
+        name: 'build:     📦️ 构建相关 | Changes that affect the build system or external dependencies',
+        emoji: ':package:'
+      },
+      {
+        value: 'ci',
+        name: 'ci:        🎡 持续集成 | Changes to our CI configuration files and scripts',
+        emoji: ':ferris_wheel:'
+      },
+      { value: 'revert', name: 'revert:    🔨 回退代码 | Revert to a commit', emoji: ':hammer:' },
+      {
+        value: 'chore',
+        name: 'chore:     ⏪️ 其他修改 | Other changes that do not modify src or test files',
+        emoji: ':rewind:'
+      }
+    ],
+    useEmoji: true,
+    emojiAlign: 'center',
+    themeColorCode: '',
+    allowCustomScopes: true,
+    allowEmptyScopes: true,
+    customScopesAlias: 'custom',
+    emptyScopesAlias: 'empty',
+    upperCaseSubject: false,
+    markBreakingChangeMode: false,
+    allowBreakingChanges: ['feature', 'fixbug'],
+    breaklineNumber: 100,
+    breaklineChar: '|',
+    skipQuestions: [],
+    issuePrefixs: [
+      // 如果使用 gitee 作为开发管理
+      { value: 'link', name: 'link:     链接 ISSUES 进行中' },
+      { value: 'closed', name: 'closed:   标记 ISSUES 已完成' }
+    ],
+    customIssuePrefixsAlign: 'top',
+    emptyIssuePrefixsAlias: 'skip',
+    customIssuePrefixsAlias: 'custom',
+    confirmColorize: true,
+    maxHeaderLength: Infinity,
+    maxSubjectLength: Infinity,
+    minSubjectLength: 0,
+    scopeOverrides: undefined,
+    defaultBody: '',
+    defaultIssues: '',
+    defaultSubject: ''
   }
 };
